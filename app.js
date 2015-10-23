dd2a0eb1965b6c2b04bceb6f670561b534e88be5
Kurt Weiberth <kurt.weiberth@gmail.com>
2015-10-23 08:41:11 -0700

    First commit

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-// styles.js
-var styles= {
+
+
+
+
+var styles = {
   button: {
     color: 'black',
     border: '1px solid black',
     borderRadius: '4px',
-    textAlign: 'center',
     margin: '20px 0',
     padding: '10px',
+    textAlign: 'center',
     width: '150px'
   },
   buttonDisabled: {
@@ -14,10 +17,23 @@ var styles= {
   }
 };
 
-// Button.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var Button = React.createClass({
 
-  
   render: function() {
 
     var buttonStyles = _.extend({},
@@ -25,21 +41,40 @@ var Button = React.createClass({
       this.props.disabled && styles.buttonDisabled
     );
     
-    return <div style={buttonStyles}>React button!</div>
+    return ( 
+      <div style={buttonStyles}>React button!</div>
+    );
   }
 
 });
 
 
+
+
+
+
+
+
+
+
+
+
+
 var App = React.createClass({
 
+  handleDisable: function() {
+    this.setState({
+      disabled: !this.state.disabled
+    });
+  },
+
   componentDidMount: function() {
-    var self = this;
-    window.onresize = function() { self.setState({ ww: window.innerWidth }); }
+    window.onresize = function() { 
+      this.setState({ ww: window.innerWidth }); 
+    }.bind(this);
   },
 
   getInitialState: function() {
-
     return {
       disabled: false,
       ww: window.innerWidth
@@ -50,15 +85,34 @@ var App = React.createClass({
 
     return (
       <div>
+
         <Button disabled={this.state.disabled} />
-        <p style={ {
-          fontSize: this.state.ww / 20,
-          opacity: this.state.ww / 1200
-        } }>hella foo</p>
+
+        <form onChange={this.handleDisable}>
+          <input type="checkbox"></input>disable
+        </form>
+
+        <span style={{
+          fontSize: this.state.ww / 8, 
+          opacity: this.state.disabled ? 1 : this.state.ww / 750
+        }}>
+          hella foo
+        </span>
+
       </div>
     );
   }
 });
+
+
+
+
+
+
+
+
+
+
 
 ReactDOM.render(
   <App />,
